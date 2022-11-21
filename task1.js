cc725ae514917d8e863e25ed216f411647299f6c
Elvinoo <elvinooo1990@gmail.com>
2022-11-21 21:01:25 +0400

    task1 done

---
+++ task1.js
@@ -0,0 +1,9 @@
+console.clear()
+const sentence = ['sense.', 'make', 'all', 'will', 'This'];
+console.log(sentence);
+
+function reverseArray(array){
+    return array.reverse();
+}
+
+console.log(reverseArray(sentence));
