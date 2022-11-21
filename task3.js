eaa88d36e19c8210a13465b09f5f8f3280cf129e
Elvinoo <elvinooo1990@gmail.com>
2022-11-21 23:54:28 +0400

    task3 done

--- task3.js
+++ task3.js
@@ -31,20 +31,13 @@ const coolStuff = [
     'family-night',
     'my room',
   ];
-  
- function justCoolStuff(arr1,arr2){
-    newarr=[];
-    len=0;
-    if(arr1.length>arr2.length){
-        len=arr2.length;
-    }else{
-        len=arr1.length
-    }
-   for (let i=0;i<len;i++){
-    if(arr1[i]==arr2[i]){
-        newarr.push(arr1[i]);
-    }
-   }
-   return newarr
+  function justCoolStuff(arr1,arr2){
+    newArr=[];
+    arr1.forEach((element1)=>arr2.forEach((element2)=>{
+        if(element1===element2){
+            newArr.push(element1);
+        }
+    }))
+    return newArr;
   }
-  console.log(justCoolStuff(myStuff,coolStuff))
+  console.log(justCoolStuff(myStuff,coolStuff));
